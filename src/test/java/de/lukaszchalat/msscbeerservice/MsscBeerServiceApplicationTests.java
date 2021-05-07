81ab504102a8e011856d14d6f754f385a4a9f2ac
LukaszChalat <komtakt@lukaszchalat.de>
2021-05-07 20:14:46 +0200

    initial commit

---
+++ src/test/java/de/lukaszchalat/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.lukaszchalat.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
